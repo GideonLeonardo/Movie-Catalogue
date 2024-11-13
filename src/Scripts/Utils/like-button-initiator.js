e9336d94d214d8159ade92b74905e8a7cd1775a6
Your Name <leonardogideon04@gmail.con>
2024-11-13 12:23:58 +0700

    percobaan

---
+++ src/Scripts/Utils/like-button-initiator.js
@@ -0,0 +1,42 @@
+import FavoriteMovieIdb from "../Data/favorite-movie-idb";
+import { createLikeButtonTemplate, createLikedButtonTemplate } from "../Views/Templates/template-creator";
+
+const LikeButtonInitiator = {
+  async init({ likeButtonContainer, movie }) {
+    this._likeButtonContainer = likeButtonContainer;
+    this._movie = movie;
+
+    await this._renderButton();
+  },
+
+  async _renderButton() {
+    const { id } = this._movie;
+
+    if (await this._isMovieExist(id)) {
+      this._renderLiked();
+    } else {
+      this._renderLike();
+    }
+  },
+
+  async _isMovieExist(id) {
+    const movie = await FavoriteMovieIdb.getMovie(id);
+    return !!movie;
+  },
+
+  _renderLike() {
+    this._likeButtonContainer.innerHTML = createLikeButtonTemplate();
+  },
+
+  _renderLiked() {
+    this._likeButtonContainer.innerHTML = createLikedButtonTemplate();
+
+    const likeButton = document.querySelector("#likeButton");
+    likeButton.addEventListener("click", async () => {
+      await FavoriteMovieIdb.deleteMovie(this._movie);
+      this._renderButton();
+    });
+  },
+};
+
+export default LikeButtonInitiator;
